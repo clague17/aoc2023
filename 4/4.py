69ebb8772da0530952fe3f8fb33b3b0a3000d3f6
Luis Clague <lclague9@gmail.com>
2023-12-04 14:27:50 -0800

    day 4 pt2

--- 4/4.py
+++ 4/4.py
@@ -18,16 +18,37 @@ def score_card(card, hand):
       points = pow(2, matches)
     return matches, points
 
-
+'''
+Returns the number of matches
+'''
+def process_card(card, hand):
+  matches = 0
+  for hand_num in hand:
+    for card_num in card:
+        if eval(hand_num) == eval(card_num):
+            matches += 1
+  return matches
 
 def main():
     inputFile = open('input.txt', 'r')
-    scores = []
-    scores = 0
+    matches = []
+    totalCards = []
     for line in inputFile:
+        totalCards.append(1)
         [card, hand] = line.split(': ')[1].split(' | ')
-        [matches, points] = score_card(card.split(), hand.split())
-        scores += points
-    return scores
+        cardList = card.split()
+        handList = hand.split()
+        number = process_card(cardList, handList)
+        matches.append(number)
+    for i in range(len(totalCards)):
+      nextCards = matches[i]
+      copies = totalCards[i]
+      # populate the next cards
+      for j in range(i + 1, len(totalCards)):
+        if nextCards == 0:
+          break
+        totalCards[j] += copies
+        nextCards -= 1
+    return sum(totalCards)
 
 print(main())
