7f63c7992f307cb26ec716edc67f46cd99c3b6ef
Luis Clague <lclague9@gmail.com>
2023-12-09 11:11:15 -0800

    trying pt2

--- 5/5.py
+++ 5/5.py
@@ -1,50 +1,84 @@
 '''
 Given this [
-  soil-to-fertilizer map:,
-  0 15 37,
-  37 52 2,
-  39 0 15
+    soil-to-fertilizer map:,
+    0 15 37,
+    37 52 2,
+    39 0 15
 ]
 Return  {}
 '''
 
-'''
-We can just do it inline, we dont need to parse each of the maps
-'''
-def parseMap(lst):
-    pass
+import tqdm
 
+# This just takes too long LOL 
 def main():
     inputFile = list(open('input.txt'))
     seeds = inputFile[0].split(': ')[1].split()
-    beforeConverting = [int(x) for x in seeds]
-    afterConverting = []
+    # will look like [(), (), ()...], where each tuple is (hasConverted, value)
+    answer = []
+    print('parsing seeds line')
+    for i in tqdm.trange(1, len(seeds), 2):
+        starting = int(seeds[i - 1])
+        offset = int(seeds[i])
+        for j in range(starting, starting + offset):
+            val = [False, j]
+            answer.append(val)
+    print('going through each of the maps')
     # parse each of the maps
-    for idx in range(2, len(inputFile)): # skipping seeds and \n
+    for idx in tqdm.trange(2, len(inputFile)): # skipping seeds and \n
         line = inputFile[idx]
         if line[0] == '\n': # we assume this is a \n case
             # we're moving on from this map to the next one
             # any numbers that havent been mapped by now stay what they were
-            afterConverting += [x for x in beforeConverting if x != -1]
-            beforeConverting = afterConverting
-            afterConverting = []
+            for i in range(len(answer)): answer[i][0] = False
         splitLine = line.split()
         if len(splitLine) == 3:
             startDestination = eval(splitLine[0])
             startSource = eval(splitLine[1])
             width = eval(splitLine[2])
-            # now go through beforeParsing and see if we have a key match
-            for i in range(len(beforeConverting)): # ideally we modify beforeConverting
-                if beforeConverting[i] == -1:
+            # now go through beforeConverting and see if we have a key match
+            for i in range(len(answer)): # ideally we modify beforeConverting
+                if answer[i][0] == True:
                     continue
-                if beforeConverting[i] in range(startSource, startSource + width):
-                    offset = beforeConverting[i] - startSource
+                if answer[i][1] in range(startSource, startSource + width):
+                    offset = answer[i][1] - startSource
                     conversion = startDestination + offset
-                    afterConverting.append(conversion)
-                    beforeConverting[i] = -1
+                    answer[i][1] = conversion
+                    answer[i][0] = True 
+    return min(answer, key=lambda x: x[1])[1]
 
-    # do a final translation
-    afterConverting += [x for x in beforeConverting if x != -1]
-    return min(afterConverting)
+
+# Another approach would be to go bottoms up
+# Since we know we want the minimum, we can go bottoms up until we have a valid path
+
+# def main_gpt():
+#     with open('ex.txt') as f:
+#         inputFile = f.readlines()
+
+#     seeds = inputFile[0].split(': ')[1].split()
+#     beforeConverting = {int(seeds[i - 1]) + j for i in range(1, len(seeds), 2) for j in range(int(seeds[i]))}
+
+#     afterConverting = []
+#     for line in inputFile[2:]:  # skipping seeds and \n
+#         if line[0] == '\n':
+#             beforeConverting = set(afterConverting)
+#             afterConverting = []
+#             continue
+
+#         splitLine = line.split()
+#         if len(splitLine) == 3:
+#             startDestination = int(splitLine[0])
+#             startSource = int(splitLine[1])
+#             width = int(splitLine[2])
+
+#             for i in list(beforeConverting):  # create a copy to iterate over
+#                 if startSource <= i < startSource + width:
+#                     offset = i - startSource
+#                     conversion = startDestination + offset
+#                     afterConverting.append(conversion)
+#                     beforeConverting.remove(i)
+
+#     afterConverting += list(beforeConverting)
+#     return min(afterConverting)
 
 print(main())
